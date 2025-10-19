22ad6df66da78af9a64cc3cf282d3114082752fd
A-Chong <longyanchong@gmail.com>
2025-10-19 19:13:06 +1100

    submit 30%

---
+++ Assets/Scripts/UI/AnimatedBorder.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedBorder : MonoBehaviour
+{
+    [SerializeField] private float pulseSpeed = 2f;
+    [SerializeField] private float minAlpha = 0.3f;
+    [SerializeField] private float maxAlpha = 1f;
+    
+    private Image[] borderDots;
+    
+    void Start()
+    {
+        // 获取所有子对象的Image组件
+        borderDots = new Image[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            borderDots[i] = transform.GetChild(i).GetComponent<Image>();
+        }
+    }
+    
+    void Update()
+    {
+        // 让所有点一起脉冲
+        float alpha = Mathf.Lerp(minAlpha, maxAlpha, 
+            (Mathf.Sin(Time.time * pulseSpeed) + 1f) / 2f);
+        
+        foreach (Image dot in borderDots)
+        {
+            if (dot != null)
+            {
+                Color color = dot.color;
+                color.a = alpha;
+                dot.color = color;
+            }
+        }
+    }
+}
